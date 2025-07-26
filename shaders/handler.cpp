fccfb1aaa3f1fdf929703fc0b8a14df5227df529
FeAuCo <vladikyudin09@gmail.com>
2025-07-26 03:04:10 +0300

    shader class added

--- shaders/handler.cpp
+++ shaders/handler.cpp
@@ -1,5 +1,57 @@
 #include "handler.h"
 
+void handler::Shader::setBool(const std::string& name, bool value){         
+    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), (int)value); 
+}
+
+void handler::Shader::setInt(const std::string& name, int value){
+    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), value);
+}
+
+void handler::Shader::setFloat(const std::string& name, float value){
+    glUniform1f(glGetUniformLocation(this->ID, name.c_str()), value);
+}
+
+void handler::Shader::setVec2(const std::string &name, const glm::vec2 &value){ 
+    glUniform2fv(glGetUniformLocation(this->ID, name.c_str()), 1, &value[0]); 
+}
+
+void handler::Shader::setVec2(const std::string& name, float x, float y){ 
+    glUniform2f(glGetUniformLocation(this->ID, name.c_str()), x, y); 
+}
+
+void handler::Shader::setVec3(const std::string& name, const glm::vec3 &value){ 
+    glUniform3fv(glGetUniformLocation(this->ID, name.c_str()), 1, &value[0]); 
+}
+
+void handler::Shader::setVec3(const std::string& name, float x, float y, float z){ 
+    glUniform3f(glGetUniformLocation(this->ID, name.c_str()), x, y, z); 
+}
+
+void handler::Shader::setVec4(const std::string& name, const glm::vec4 &value){ 
+    glUniform4fv(glGetUniformLocation(this->ID, name.c_str()), 1, &value[0]); 
+}
+
+void handler::Shader::setVec4(const std::string& name, float x, float y, float z, float w){ 
+    glUniform4f(glGetUniformLocation(this->ID, name.c_str()), x, y, z, w); 
+}
+
+void handler::Shader::setMat2(const std::string& name, const glm::mat2& mat){
+    glUniformMatrix2fv(glGetUniformLocation(this->ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+}
+
+void handler::Shader::setMat3(const std::string& name, const glm::mat3& mat) {
+    glUniformMatrix3fv(glGetUniformLocation(this->ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+}
+
+void handler::Shader::setMat4(const std::string& name, const glm::mat4& mat){
+    glUniformMatrix4fv(glGetUniformLocation(this->ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+}
+
+void handler::Shader::use(){
+    glUseProgram(this->ID);
+}
+
 std::string handler::parse(const std::string& filePath){
     std::ifstream stream(filePath, std::ios::binary);
     
--- shaders/handler.h
+++ shaders/handler.h
@@ -5,8 +5,42 @@
 #include <sstream>
 #include "GLEW/glew.h"
 #include "GLFW/glfw3.h"
-#include "../shaders/fragment_vertex/fragment_vertex.h"
+#include "fragment_vertex/fragment_vertex.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 namespace handler{
+    class Shader{
+        public: 
+            unsigned int ID;
+
+            void setBool(const std::string& name, bool value);
+
+            void setInt(const std::string& name, int value);
+
+            void setFloat(const std::string& name, float value);
+
+            void setVec2(const std::string& name, const glm::vec2& value);
+ 
+            void setVec2(const std::string& name, float x, float y);
+ 
+            void setVec3(const std::string& name, const glm::vec3& value);
+ 
+            void setVec3(const std::string& name, float x, float y, float z);
+ 
+            void setVec4(const std::string& name, const glm::vec4& value);
+ 
+            void setVec4(const std::string& name, float x, float y, float z, float w);
+ 
+            void setMat2(const std::string& name, const glm::mat2& mat);
+ 
+            void setMat3(const std::string& name, const glm::mat3& mat);
+ 
+            void setMat4(const std::string& name, const glm::mat4& mat);
+
+            void use();
+    };
+
     std::string parse(const std::string& filePath);
 }
